160289060fbf71a1e113302229fd6c2e53897254
Theo Chan <chan.theodore1@gmail.com>
2021-01-21 11:19:12 -0800

    Player and enemy added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 
+import random
 import pygame
 
 # ----- CONSTANTS
@@ -10,12 +11,38 @@ SKY_BLUE = (95, 165, 228)
 WIDTH = 800
 HEIGHT = 600
 TITLE = "Final game"
+NUM_RECT = 3
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
         self.image = pygame.image.load("./images/link.png")
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Move the player with the mouse"""
+        self.rect.center = pygame.mouse.get_pos()
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./images/wall.png")
+
+        self.rect = self.image.get_rect()
+
+        self.x_vel = 3
+
+    def update(self):
+        """Move rectangle side to side"""
+        self.rect.x += self.x_vel
+
+        # keep enemy in the screen
+        if self.rect.right > WIDTH or self.rect.left < 0:
+            self.x_vel *= -1
+
 
 def main():
     pygame.init()
@@ -29,6 +56,17 @@ def main():
     done = False
     clock = pygame.time.Clock()
 
+    # Sprite groups
+    all_sprites = pygame.sprite.RenderUpdates()
+
+    # --- enemy
+    enemy = Enemy()
+    all_sprites.add(enemy)
+
+    # --- player
+    player = Player()
+    all_sprites.add(player)
+
     # ----- MAIN LOOP
     while not done:
         # -- Event Handler
@@ -40,6 +78,8 @@ def main():
 
         # ----- DRAW
         screen.fill(BLACK)
+        all_sprites.draw(screen)
+
 
         # ----- UPDATE
         pygame.display.flip()
